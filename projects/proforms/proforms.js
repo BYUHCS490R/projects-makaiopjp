db4a80309a3da1ddba5b7c66ae041e4238fe6ec0
makaio primacio <makaiop.jp@gmail.com>
2026-03-30 11:40:44 -1000

    add scrib

---
+++ projects/proforms/proforms.js
@@ -0,0 +1,43 @@
+document.getElementById('myform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    //alert("Form Submitted");
+    const fullname = document.getElementById('fullname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    
+    if (!fullname || !email) {
+        alert("You need a name and email.");
+        return;
+    }
+
+    if (!age || age <18) {
+        alert("You need to be 18");
+        return;
+    }
+
+    const formData = {
+        name: fullname,
+        email: email,
+        password: password,
+        age: age,
+    }
+
+    console.log(formData);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            alert("Form submitted successfully!");
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+            //document.getElementById('myForm').reset();
+            document.getElementById('myform').innerHTML = '';
+            document.getElementById('message').innerText = response.message;
+        } else if (xhr.readyState === 4) {
+            alert("Error submitting form.");
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+});
