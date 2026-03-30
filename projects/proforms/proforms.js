ca2198b06b5b43ea64fa9505a7c5e48de5d72808
makaio primacio <makaiop.jp@gmail.com>
2026-03-30 11:50:48 -1000

    add js

--- projects/proforms/proforms.js
+++ projects/proforms/proforms.js
@@ -7,12 +7,12 @@ document.getElementById('myform').addEventListener('submit', function(event) {
     const age = document.getElementById('age').value;
     
     if (!fullname || !email) {
-        alert("You need a name and email.");
+        alert("You need a name and email.")
         return;
     }
 
     if (!age || age <18) {
-        alert("You need to be 18");
+        alert("You need to be 18")
         return;
     }
 
