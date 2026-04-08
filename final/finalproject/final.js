050accbdab7f9a800af91c40fbf5d784fc6c46e0
makaio primacio <makaiop.jp@gmail.com>
2026-04-07 17:52:31 -1000

    add forms

---
+++ final/finalproject/final.js
@@ -0,0 +1,35 @@
+document.getElementById('myform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    //alert("Form Submitted");
+    const fullname = document.getElementById('fullname').value;
+    const email = document.getElementById('email').value;
+
+    
+    if (!fullname || !email) {
+        alert("You need a name and email.")
+        return;
+    }
+
+    const formData = {
+        name: fullname,
+        email: email,
+    }
+
+    console.log(formData);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            alert("Form submitted successfully!");
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+            //document.getElementById('myForm').reset();
+            document.getElementById('myform').innerHTML = '';
+            document.getElementById('message').innerText = response.message;
+        } else if (xhr.readyState === 4) {
+            alert("Error submitting form.");
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+});
